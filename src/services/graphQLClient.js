e9be352182740fe6160c4141287c3b3a1b00a54d
mmtnz <mariomlafuente@gmail.com>
2025-03-26 10:30:32 +0100

    added appsyinc option

--- src/services/graphQLClient.js
+++ src/services/graphQLClient.js
@@ -1,23 +1,43 @@
 import { ApolloClient, InMemoryCache, HttpLink, split } from "@apollo/client";
-// import { WebSocketLink } from "@apollo/client/link/ws";
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
-// import { createAuthLink } from "aws-appsync-auth-link"; // Required for AppSync
-// import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link"; // Required for AppSync
+import { createAuthLink } from "aws-appsync-auth-link"; // Required for AppSync
+import { createSubscriptionHandshakeLink } from "aws-appsync-subscription-link"; // Required for AppSync
 
-// const useAppSync = process.env.REACT_APP_USE_APPSYNC === "true";
+const useAppSync = process.env.REACT_APP_USE_APPSYNC === "true";
 
 
 export const createApolloClient = (userName, conversationId=null) => {
-        const httpLink = new HttpLink({
+
+    // Connecting to an aws based backend with appsync
+    if (useAppSync) {
+        const appsyncConfig = {
+            url: process.env.REACT_APP_APPSYNC_ENDPOINT,
+            region: process.env.REACT_APP_AWS_REGION,
+            auth: {
+                type: "API_KEY", // or "AMAZON_COGNITO_USER_POOLS"
+                apiKey: process.env.REACT_APP_APPSYNC_API_KEY,
+            },
+            disableOffline: true,
+        };
+
+        return new ApolloClient({
+            link: createAuthLink(appsyncConfig).concat(
+                createSubscriptionHandshakeLink(appsyncConfig)
+            ),
+            cache: new InMemoryCache(),
+        });
+
+    }
+
+
+    // Connect to local Apollo Server Express Backend
+
+    const httpLink = new HttpLink({
         uri: process.env.REACT_APP_GRAPHQL_ENDPOINT, // Apollo Server URL
     });
 
-    // const wsLink = new GraphQLWsLink({
-    //     uri: process.env.REACT_APP_GRAPHQL_WS, // Apollo WebSocket URL
-    //     options: { reconnect: true },
-    // });
     // WebSocket Link for Subscriptions
     const wsLink = new GraphQLWsLink(
         createClient({
@@ -51,18 +71,3 @@ export const createApolloClient = (userName, conversationId=null) => {
     return client;          
 }
 
-
-// AWS AppSync Configuration
-// if (useAppSync) {
-//   const awsConfig = {
-//     url: process.env.REACT_APP_APPSYNC_ENDPOINT,
-//     region: process.env.REACT_APP_AWS_REGION,
-//     auth: {
-//       type: "API_KEY", // or "AMAZON_COGNITO_USER_POOLS"
-//       apiKey: process.env.REACT_APP_APPSYNC_API_KEY,
-//     },
-//   };
-
-//   httpLink = createAuthLink(awsConfig);
-//   wsLink = createSubscriptionHandshakeLink(awsConfig, httpLink);
-// }
